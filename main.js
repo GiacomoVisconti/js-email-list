2fb3359802ad09eeb4d374e901703ed4c4b520a4
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-16 16:51:35 +0200

    add comments

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
+//Dichiaro le costanti 
 const random_number = 'https://flynn.boolean.careers/exercises/api/random/mail'
 const list = document.querySelector('ul')
 const email_button_El = document.getElementById('button_mail')
 
-
+//con il ciclo for ripeto l'iterazione 10 volte al caricamento della pagina
 for (let i = 0; i < 10; i++) {
-        
+      
+    //interrogo il server richiedendo le mail random
     fetch(random_number)
     .then(response => response.json())
     .then(data => {
+
+    //creo un elemento della dom appoggiandolo in una variabile
     const list_element =`<li class="list-group-item"> ${data.response} </li> `
         console.log(list_element);
 
+        //aggiungo l'elemento dopo quello inserito precedentemente
         list.insertAdjacentHTML('beforeend', list_element)
         
     
@@ -18,10 +23,12 @@ for (let i = 0; i < 10; i++) {
 
 }
 
+
+//all'evento click del bottone, ripeto le iterazioni andando a sostituire gli elementi precedenti creati
 email_button_El.addEventListener('click', (e) => {
     e.preventDefault()
     list.innerHTML = ''
-    list
+    
     for (let i = 0; i < 10; i++) {
         
         fetch(random_number)
