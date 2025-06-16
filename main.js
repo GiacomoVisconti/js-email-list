87bdc4bcfad4f36ee3b857640b2d2725a3d39a06
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-16 14:59:02 +0200

    add button to print the list  (BONUS)

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const random_number = 'https://flynn.boolean.careers/exercises/api/random/mail'
 const list = document.querySelector('ul')
+const email_button_El = document.getElementById('button_mail')
 
-for (let i = 0; i < 10; i++) {
-    
-    fetch(random_number)
-    .then(response => response.json())
-    .then(data => {
-    const list_element =`<li> ${data.response} </li> `
-        console.log(list_element);
+email_button_El.addEventListener('click', (e) => {
+    e.preventDefault()
 
-        list.insertAdjacentHTML('beforeend', list_element)
+
+    for (let i = 0; i < 10; i++) {
         
+        fetch(random_number)
+        .then(response => response.json())
+        .then(data => {
+        const list_element =`<li class="list-group-item"> ${data.response} </li> `
+            console.log(list_element);
     
-    })
+            list.insertAdjacentHTML('beforeend', list_element)
+            
+        
+        })
+    
+    }
+})
 
-}
