6c46fc3db0ed9d657589812308ac4b3b235ccccb
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-16 14:51:22 +0200

    print the list in DOM

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const random_number = 'https://flynn.boolean.careers/exercises/api/random/mail'
-
+const list = document.querySelector('ul')
 
 for (let i = 0; i < 10; i++) {
     
     fetch(random_number)
     .then(response => response.json())
     .then(data => {
-        console.log(data.response);
+    const list_element =`<li> ${data.response} </li> `
+        console.log(list_element);
+
+        list.insertAdjacentHTML('beforeend', list_element)
+        
     
-})
+    })
 
 }
